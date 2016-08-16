2cbec1ad9f1c14de0d5e16968c91ad4c3fc0483f
wink94 <mwindula16@gmail.com>
2016-08-16 18:49:30 +0530

    add c1 c2

--- ConsoleApplication1/Class2.cs
+++ ConsoleApplication1/Class2.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Class1
+    class Class2
     {
         string name;
         string assssdd;
 
-        public Class1(string n,string ass)
+        public Class2(string n, string ass)
         {
             name = n;
             assssdd = ass;
