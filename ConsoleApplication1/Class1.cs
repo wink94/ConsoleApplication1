c966761b9a3dd4666989ebb8187e5d77922c77da
wink94 <mwindula16@gmail.com>
2016-08-16 18:37:20 +0530

    uguiguigiug

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        string name;
+        string assssdd;
+
+        public Class1(string n,string ass)
+        {
+            name = n;
+            assssdd = ass;
+
+        }
+    }
+}
